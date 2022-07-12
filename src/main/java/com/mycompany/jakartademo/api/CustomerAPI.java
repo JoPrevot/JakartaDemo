aec61ab360ad7e97b7e3353613b003a1cedf685b
Jordan Prevot <jordan.prevot59@gmail.com>
2022-07-12 13:56:11 +0200

    Netbeans - JakartaDemo - Get customer by id

--- src/main/java/com/mycompany/jakartademo/api/CustomerAPI.java
+++ src/main/java/com/mycompany/jakartademo/api/CustomerAPI.java
@@ -3,12 +3,14 @@ package com.mycompany.jakartademo.api;
 import com.mycompany.jakartademo.business.Customer;
 import com.mycompany.jakartademo.business.Directory;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -38,8 +40,26 @@ public class CustomerAPI {
         public void postCustomer(Customer customer,@Context HttpServletRequest request)
         {
             Directory directory = (Directory) request.getSession().getAttribute("directory");
+            if(directory == null)
+            {
+                directory = new Directory();
+            }
             directory.add(customer);
             request.getSession().setAttribute("directory",directory);
+        }
+        
+        @Path("/{id}")
+        @GET()
+        @Produces({MediaType.APPLICATION_JSON})
+        public Customer getById(@PathParam("id") Long id,@Context HttpServletRequest request)
+        {
+             Directory directory = (Directory) request.getSession().getAttribute("directory");
+            if(directory == null)
+            {
+                directory = new Directory();
+            }
+            Optional<Customer> customerOptionnal = directory.findById(id);
+            return customerOptionnal.get();
         }
 
 }
